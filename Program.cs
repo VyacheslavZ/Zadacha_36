378fb658d3e38f506dd24bc6a34f632e7bb6a3d4
Slava <d-color.38@mail.ru>
2022-09-16 01:19:47 +0800

    dz36

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿int[] FillArray(int size)
+{
+    int[] mas = new int[size];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(0, 20);
+    }
+    return mas;
+}
+
+void PrintArray(int[] mas)
+{
+    Console.Write("[");
+    for (int i = 0; i < mas.Length - 1; i++)
+    {
+        Console.Write(mas[i] + ", ");
+    }
+    Console.WriteLine(mas[mas.Length - 1] + "]");
+}
+
+int SumUnevenCell(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        result += arr[i];
+    }
+    return result;
+}
+
+try
+{
+    System.Console.Write("Введите размерность массива: ");
+    int k = Convert.ToInt32(Console.ReadLine());
+    int[] array = FillArray(k);
+    PrintArray(array);
+    Console.WriteLine("Сумма элементов с нечетными индексами = " + SumUnevenCell(array));
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целое положительное число!");
+}
